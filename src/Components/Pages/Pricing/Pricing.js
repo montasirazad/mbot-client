77f556be2da6e002f3dfd66a119da1b1f4610a61
unknown <montasirazad@gmail.com>
2023-06-27 14:22:48 +0600

    pricing section done

--- src/Components/Pages/Pricing/Pricing.js
+++ src/Components/Pages/Pricing/Pricing.js
@@ -12,12 +12,138 @@ const Pricing = () => {
 
                 <div className='essential-div'>
                     <h1>Essential</h1>
+                    <br /><br />
+                    <p>The perfect plan for getting started and experience
+                        all the amazing features Nando AI has to offer.</p>
+                    <br /><br />
+                    <h2>$47/Month</h2>
+                    <br /> <br />
+                    <table>
+
+                        <tbody>
+                            <tr>
+                                <td>50,000 words </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>$0,00071 per word </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>Nando Chat</td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando Translate
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando WhatsApp
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                        </tbody>
+
+
+                    </table>
+
+
+
+
+                    <button>Try 5 days free trial</button>
                 </div>
+
                 <div className='plus-div'>
                     <h1>Plus</h1>
+                    <br /><br />
+                    <p>The perfect plan for getting started and experience
+                        all the amazing features Nando AI has to offer.</p>
+                    <br /><br />
+                    <h2>$47/Month</h2> <br /> <br />
+                    <table>
+
+                        <tbody>
+                            <tr>
+                                <td>50,000 words </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>$0,00071 per word </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>Nando Chat</td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando Translate
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando WhatsApp
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                        </tbody>
+
+
+                    </table>
+
+
+
+
+                    <button>Try 5 days free trial</button>
                 </div>
                 <div className='professional-div'>
                     <h1>Professional</h1>
+                    <br /><br />
+                    <p>The perfect plan for getting started and experience
+                        all the amazing features Nando AI has to offer.</p>
+                    <br /><br />
+                    <h2>$47/Month</h2>
+                    <br /> <br />
+                    <table>
+
+                        <tbody>
+                            <tr>
+                                <td>50,000 words </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>$0,00071 per word </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>Nando Chat</td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando Translate
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    Nando WhatsApp
+                                </td>
+                                <td><span>&#9989;</span></td>
+                            </tr>
+                        </tbody>
+
+
+                    </table>
+
+
+
+
+                    <button>Try 5 days free trial</button>
                 </div>
 
             </div>
