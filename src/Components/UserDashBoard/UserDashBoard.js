a6cd56f414017f89e6690d5a24c2acd5804b1e30
unknown <montasirazad@gmail.com>
2023-07-08 17:05:55 +0600

    added side nav menu

--- src/Components/UserDashBoard/UserDashBoard.js
+++ src/Components/UserDashBoard/UserDashBoard.js
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './UserDashBoard.css';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import NavBar from '../Shared/Navbar/NavBar';
 import Footer from '../Shared/Footer/Footer';
 
 const UserDashBoard = () => {
+
+    const menuRef = useRef()
+
+    const handleMenuButton = () => {
+        menuRef.current.classList.toggle('menuHide')
+    }
+
     return (
         <div>
-            {/* <div className='header-div'>
+            <div className='header-div'>
+                <Link to='/'>Home</Link>
                 <h2>Gengenie</h2>
-            </div> */}
-            <NavBar />
+                <button onClick={handleMenuButton}><span style={{ fontSize: '30px', cursor: 'pointer' }}>&#9776;</span></button>
+            </div>
+            {/* <NavBar /> */}
             <div className="dashboard-container">
-                <div className='dashboard-links'>
 
+                <div className='dashboard-links' ref={menuRef}>
+                    <button className='close-btn' onClick={handleMenuButton}>
+                        <span style={{ fontSize: '30px', cursor: 'pointer' }}>
+                            &times;
+                        </span>
+                    </button>
                     <NavLink to='/user/dashboard'>New Post</NavLink>
                     <NavLink to='/user/dashboard/website-list'>website list</NavLink>
                     <NavLink to='/'>Option 3</NavLink>
@@ -26,9 +40,9 @@ const UserDashBoard = () => {
             </div>
 
             <br />
-           <div>
+            <div>
 
-           </div>
+            </div>
         </div>
     );
 };
