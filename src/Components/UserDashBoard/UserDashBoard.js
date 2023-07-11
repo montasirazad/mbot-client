74fe43ed37383b08f6891dabd8e8d527ff08fb34
unknown <montasirazad@gmail.com>
2023-07-11 18:44:37 +0600

    prepared layout

--- src/Components/UserDashBoard/UserDashBoard.js
+++ src/Components/UserDashBoard/UserDashBoard.js
@@ -10,50 +10,45 @@ const UserDashBoard = () => {
         menuRef.current.classList.toggle('menuShow')
     }
 
+    /* <div className='header-div'>
+               <span onClick={handleMenuButton} className='open-btn'>
+                   &#9776;
+               </span>
+
+
+               <h2>Gengenie</h2>
+               <div>
+                   <p>hi, ....</p>
+               </div>
+           </div> */
+
     return (
-        <div>
-            <div className='header-div'>
-                <span onClick={handleMenuButton} className='open-btn'>
-                    &#9776;
+        <div className="dashboard-container">
+
+            <div className='dashboard-links' ref={menuRef}>
+                <h1>G-1</h1>
+                <span onClick={handleMenuButton}
+                    className='close-btn'>
+                    &times;
+
                 </span>
 
-
-                <h2>Gengenie</h2>
-                <div>
-                    <p>hi, ....</p>
-                </div>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/user/dashboard'>New Post  +</NavLink>
+                <NavLink to='/user/dashboard/website-list'>website list</NavLink>
+                <NavLink to='/'>Option 3</NavLink>
+                <NavLink to='/'>Option 4</NavLink>
             </div>
 
 
-
-            <div className="dashboard-container">
-
-                <div className='dashboard-links' ref={menuRef}>
-
-                    <span onClick={handleMenuButton}
-                        className='close-btn'>
-                        &times;
-
-                    </span>
-
-                    <NavLink to='/'>Home</NavLink>
-                    <NavLink to='/user/dashboard'>New Post  +</NavLink>
-                    <NavLink to='/user/dashboard/website-list'>website list</NavLink>
-                    <NavLink to='/'>Option 3</NavLink>
-                    <NavLink to='/'>Option 4</NavLink>
-                </div>
-                <div>
-                    {/* <h1>Test</h1> */}
-                    <Outlet />
-                </div>
-            </div>
-
-            <br />
-            <div>
-
+            <div className='outlet-container'>
+                <h1>Test</h1>
+                <Outlet />
             </div>
         </div>
+
     );
 };
 
 export default UserDashBoard;
+
